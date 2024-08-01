a8ef10abca4ec45aa6b10b21a9c84f74d81c3c64
Betaf <beta.falc2@gmail.com>
2024-08-01 16:03:41 +0200

    fixed background size and gameover img

--- gameover.js
+++ gameover.js
@@ -21,7 +21,7 @@ class Nanas {
         this.domElm.className = "bomb";
 
         const img = document.createElement("img");
-        img.src = 'nanas3-santo.png';
+        img.src = 'gatos.gif';
         img.style.width = '180%';
         img.style.height ='180%';
         img.style.objectFit = 'contain';
