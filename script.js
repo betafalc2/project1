93366b3c31c205bfd80a986d174c263316dcdf5d
Betaf <beta.falc2@gmail.com>
2024-08-01 11:34:00 +0200

    added nanas as a player

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ let timeLeft = 60;
 
 //timer//
 start.addEventListener('click', () => {
-    new Clickie();
+    new ClickieMinina();
+    new ClickieNanas();
 
     setInterval(() => {
         countingTime.innerHTML = timeLeft--;
@@ -31,10 +32,10 @@ start.addEventListener('click', () => {
 
 
 //click thingies//
-class Clickie {
+class ClickieMinina {
     constructor() {
-        this.width = 25;
-        this.height = 30;
+        this.width = 20;
+        this.height = 25;
         this.positionX = Math.floor(Math.random() * (100 - this.width + 1));
         this.positionY = Math.floor(Math.random() * (100 - this.height + 1));
         this.createDomElm();
@@ -78,10 +79,57 @@ class Clickie {
 
 }
 
+class ClickieNanas {
+    constructor() {
+        this.width = 20;
+        this.height = 25;
+        this.positionX = Math.floor(Math.random() * (100 - this.width + 1));
+        this.positionY = Math.floor(Math.random() * (100 - this.height + 1));
+        this.createDomElm();
+        this.clickClikie();
+    }
+
+    createDomElm() {
+        this.domElm = document.createElement("div");
+        this.domElm.className = "clickies";
+
+        const img = document.createElement("img");
+        img.src = 'nanas3-santo.png';
+        img.style.width = '100%';
+        img.style.height ='100%';
+        img.style.objectFit= 'contain';
+
+        this.domElm.style.width = this.width + "vw";
+        this.domElm.style.height = this.height + "vh";
+        this.domElm.style.left = this.positionX + "vw";
+        this.domElm.style.bottom = this.positionY + "vh";
+
+        this.domElm.appendChild(img);
+        board.appendChild(this.domElm);
+    }
+
+    clickClikie() { //update score and move to random position//
+        this.domElm.addEventListener('click', () => {
+            score++;
+            countersArr.forEach(element => {
+                element.innerHTML = score;
+            });
+
+            this.positionX = Math.floor(Math.random() * (100 - this.width + 1));
+            this.positionY = Math.floor(Math.random() * (100 - this.height + 1));
+
+            this.domElm.style.left = this.positionX + "vw";
+            this.domElm.style.bottom = this.positionY + "vh";
+        })
+
+    }
+
+}
+
 class Bomb {
     constructor() {
-        this.width = 25;
-        this.height = 30;
+        this.width = 20;
+        this.height = 25;
         this.positionX = Math.floor(Math.random() * (100 - this.width + 1));
         this.positionY = Math.floor(Math.random() * (100 - this.height + 1));
         this.createDomElm();
